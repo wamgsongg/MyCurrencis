755ca20aca2d823a886a5b3eaccc019a0b9d99a2
王嵩 <31701018@stu.zucc.edu.cn>
2020-07-02 11:34:44 +0800

    初始界面设计

---
+++ app/src/main/java/com/zucc/ws31701018/JSONPareser.java
@@ -0,0 +1,25 @@
+package com.zucc.ws31701018;
+
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class JSONPareser {
+    static InputStream is = null;
+    static JSONObject ReturnJsonObject = null;
+    static String RawJsonObject = "";
+    public JSONPareser(){}
+
+    public JSONObject getJSONFromUrl(String url) {
+//        try{
+//          //  URL urlobj = new URL(url);
+//           // HttpURLConnection conn = (HttpURLConnection)urlobj.openConnection()
+//        }
+//        catch(){
+//
+//        }
+        return null;
+    }
+}
